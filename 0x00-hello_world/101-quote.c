790b50aa6b8c366f10174abd07977ca6b62b235b
salamaayuba <salamaayuba111@gmail.com>
2022-09-12 04:00:44 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,29 @@
+  * File: 101-quote.c                                                                                                            
+  *
+  *  * AUTH: Salama Ayuba                                                                                                           
+  *
+  *   */                                                                                                                             
+
+                                                                                                                                  
+
+#include <unistd.h>                                                                                                             
+
+ /**
+  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+  *
+  * followed by a new line to standard error
+  *
+  * return: always 1 
+  */         
+
+                                                                              
+  int main(void)                                                                                                                  
+
+{                                                                                                                              
+	        write(2,                                                                                                                                        
+	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",                                        
+        59);                                                                          
+
+return: (1)                                                                                                                     
+}        
+
