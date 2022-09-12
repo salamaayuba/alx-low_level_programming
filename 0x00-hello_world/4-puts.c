5834c0f502682a6acbe0bc24f80f172e48a7f39a
salamaayuba <salamaayuba111@gmail.com>
2022-09-12 01:16:00 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,23 @@
- #include <stdio.h>
+#include <stdio.h>
 
 /**
+ * main - prints the string in the put function
  *
- *   main - prints the string in the put function
+ * Description: using the main function
  *
- *   Description: using the main function
+ * This program prints "programing is like building in multilingual puzzle"
  *
- *   This program prints "programing is like building in multilingual puzzle"
+ * Return: 0
  *
- *    Return: 0
- *    
  */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programing is like building in multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
+
