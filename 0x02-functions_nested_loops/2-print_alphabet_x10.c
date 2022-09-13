883c3affb9bcb9ceac01c6d498ca2b2d0ed48936
salamaayuba <salamaayuba111@gmail.com>
2022-09-13 09:25:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 void print_alphabet_x10(void)
 {
 char c, i;
-for (c = 'a'; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-putchar('\n');
+_putchar('\n');
 }
 }
