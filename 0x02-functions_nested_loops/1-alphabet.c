b0bf6b1bd52f50744cd9f348e8de2c3975204c6e
salamaayuba <salamaayuba111@gmail.com>
2022-09-13 08:01:56 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
-print_alphabet - function to print abc
+* print_alphabet - function to print abc
 *
-*Return: 0
+* Return: 0
 */
 
-void print_alphabet(void)
 {
 
 char c;
@@ -13,10 +12,10 @@ char c;
 for (c = 'a'; c <= 'z'; c++)
 
 {
-
 _putchar(c);
 
 }
-_putchar('\n');
+
+_putchar(\'n');
 
 }
