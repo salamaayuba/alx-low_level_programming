900efd951e93ff170867be591bb44fe7bb733f9d
salamaayuba <salamaayuba111@gmail.com>
2022-09-17 06:05:58 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - program that prints all posible combination of 2 digits
+ * Return: 0
+ */
+intmain(void)
+{
+	int c = 0;
+	int f_d;
+	int l_d;
+
+	while (c <= 99)
+	{
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+
+		if (f_d < l_d)
+		{
+			putchar(F_d);
+			putchar(l_d);
+			
+			if (c != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
