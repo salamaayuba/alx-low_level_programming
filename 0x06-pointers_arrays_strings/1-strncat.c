c6ae26682ed4ad241f0fb43957842c696646cfeb
salamaayuba <salamaayuba111@gmail.com>
2022-09-21 14:01:05 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,14 @@
  * Return: returns new concatenated string
  */
 
-char _strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index 0; src[index] && index < n; index++)
+	for (index; 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
