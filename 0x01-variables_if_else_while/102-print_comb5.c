94e9bbb302493451eb4f526767a155c1c1793b70
salamaayuba <salamaayuba111@gmail.com>
2022-09-22 12:10:13 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
+ * main - Prints all possible combinations of three different digits
+ * in ascending order, separated by a comma followed by a space.
  *
- * Return: Always success
+ * Return: Always 0.
  */
 int main(void)
 {
-	int num1, num2;
+	int digit1, digit2, digit3;
 
-	for (num1 = 0; num1 <= 98; num2++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
-		
-				continue;
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
 
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
 
