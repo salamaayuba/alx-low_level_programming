099d71e2f47a6163eab581a2b4c19620a2126330
salamaayuba <salamaayuba111@gmail.com>
2022-10-21 00:33:14 -0700

    0x012-singly-linked-lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,3 +30,22 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (0);
 }
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
+}
